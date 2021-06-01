28f0a31216d1f29793870c4b5bca57509f48f9ed
Fares <fares.osman@outlook.com>
2021-05-31 22:54:11 -0400

    set up 203

---
+++ linked_list/203.remove_linked_list_elements.js
@@ -0,0 +1,26 @@
+// Given the head of a linked list and an integer val,
+// remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+// Example 1:
+
+// Input: head = [1,2,6,3,4,5,6], val = 6
+// Output: [1,2,3,4,5]
+// Example 2:
+
+// Input: head = [], val = 1
+// Output: []
+// Example 3:
+
+// Input: head = [7,7,7,7], val = 7
+// Output: []
+
+const Node = require("./linked_list_implement").Node;
+
+const removeElements = (head, val) => {};
+
+const node1 = new Node(6);
+node1.next = new Node(2);
+node1.next.next = new Node(6);
+node1.next.next.next = new Node(4);
+
+reemoveElements(node1, 6);
