66d4ffa14dc3d87a227f5b8603f3e6f5d7841db5
Fares <fares.osman@outlook.com>
2021-06-12 13:26:22 -0400

    Finish 1351

--- arrays/1351_count_num_of_negatives.js
+++ arrays/1351_count_num_of_negatives.js
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
 
 const countNegatives = (grid) => {
+  //O(n^2) time and O(1) space
   let count = 0;
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
