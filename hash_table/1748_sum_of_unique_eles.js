20c3a2cf1b13b116b3dafca60086a7a55388ab92
Fares <fares.osman@outlook.com>
2021-06-14 22:40:32 -0400

    Finish 1748

---
+++ hash_table/1748_sum_of_unique_eles.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/sum-of-unique-elements/
+
+const sumOfUnique = (nums) => {
+  let numsFreq = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (numsFreq[nums[i]]) {
+      numsFreq[nums[i]] += 1;
+    } else {
+      numsFreq[nums[i]] = 1;
+    }
+  }
+
+  let count = 0;
+  for (let num in numsFreq) {
+    if (numsFreq[num] === 1) {
+      count += parseInt(num);
+    }
+  }
+
+  return count;
+};
+
+console.log(sumOfUnique([1, 1, 1, 1]));
