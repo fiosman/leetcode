41f995d7903cf31ac49257cdc70adb557ba182ae
Fares <fares.osman@outlook.com>
2021-06-16 00:16:01 -0400

    Attempt #1 at 535

--- hash_table/535_encode_and_decode_url.js
+++ hash_table/535_encode_and_decode_url.js
@@ -1,7 +1,23 @@
 // https://leetcode.com/problems/encode-and-decode-tinyurl/
 
 class Solution {
-  encode(url) {}
+  constructor() {
+    this.hash = {};
+  }
+  encode(url) {
+    this.hash["shortUrl"] = `http://tinyurl.com/${Math.random()
+      .toString(36)
+      .slice(2)}`;
+    this.hash["longUrl"] = url;
+    return this.hash["shortUrl"];
+  }
 
-  decode(tiny) {}
+  decode(tiny) {
+    return this.hash["longUrl"];
+  }
 }
+
+const s = new Solution();
+console.log(s.hash);
+s.encode("https://leetcode.com/problems/design-tinyurl");
+console.log(s.hash);
