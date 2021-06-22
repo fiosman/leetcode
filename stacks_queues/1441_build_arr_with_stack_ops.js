a8164c573b45f32e67b623224c6fca525d8657bc
Fares <fares.osman@outlook.com>
2021-06-21 21:39:23 -0400

    Finish 1441

--- stacks_queues/1441_build_arr_with_stack_ops.js
+++ stacks_queues/1441_build_arr_with_stack_ops.js
@@ -1,3 +1,19 @@
 // https://leetcode.com/problems/build-an-array-with-stack-operations/
 
-const buildArray = (target, n) => {};
+const buildArray = (target, n) => {
+  let outputArr = [];
+  let targetLength = target.length;
+  for (let i = 1; i <= n; i++) {
+    if (targetLength === 0) break;
+    if (!target.includes(i)) {
+      outputArr.push("Push", "Pop");
+    } else {
+      outputArr.push("Push");
+      targetLength--;
+    }
+  }
+
+  return outputArr;
+};
+
+console.log(buildArray([2, 3, 4], 4));
