32a974b87a254705f2c9e7c7a7946dc624b5c213
Fares <fares.osman@outlook.com>
2021-06-27 20:27:20 -0400

    Finish 145

--- trees/145_binary_tree_postorder.js
+++ trees/145_binary_tree_postorder.js
@@ -1,3 +1,13 @@
 // https://leetcode.com/problems/binary-tree-postorder-traversal/
 
-const postorderTraversal = (root) => {};
+const postorderTraversal = (root) => {
+  if (!root) return [];
+
+  let res = [];
+
+  res = res.concat(postorderTraversal(root.left));
+  res = res.concat(postorderTraversal(root.right));
+  res.push(root.val);
+
+  return res;
+};
