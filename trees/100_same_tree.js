84c01aa571dff0797d6843189417ff26b6297961
Fares <fares.osman@outlook.com>
2021-07-02 09:59:43 -0400

    Finish 100

--- trees/100_same_tree.js
+++ trees/100_same_tree.js
@@ -17,8 +17,35 @@
 
 const Node = require("./node_class").TreeNode;
 
-const isSameTree = (root1, root2) => {
-  
+const isSameTree = (p, q) => {
+  if (!p && !q) return true;
+  let root1Q = [p];
+  let root2Q = [q];
+
+  while (root1Q.length || root2Q.length) {
+    const root1Node = root1Q.shift();
+    const root2Node = root2Q.shift();
+
+    if (!root1Node || !root2Node) return false;
+
+    if (root1Node.val != root2Node.val) return false;
+
+    if (root1Node.left && root2Node.left) {
+      root1Q.push(root1Node.left);
+      root2Q.push(root2Node.left);
+    } else if (root1Node.left != null || root2Node.left != null) {
+      return false;
+    }
+
+    if (root1Node.right && root2Node.right) {
+      root1Q.push(root1Node.right);
+      root2Q.push(root2Node.right);
+    } else if (root1Node.right != null || root2Node.right != null) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 const root1 = new Node(1);
@@ -28,3 +55,5 @@ root1.right = new Node(3);
 const root2 = new Node(1);
 root2.left = new Node(2);
 root2.right = new Node(3);
+
+console.log(isSameTree(root1, root2));
