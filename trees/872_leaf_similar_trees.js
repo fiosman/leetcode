c8ac3be4433d90cda2b947030a1a1c95d962633f
Fares <fares.osman@outlook.com>
2021-07-03 23:28:31 -0400

    set up dfs boilerplaate code

--- trees/872_leaf_similar_trees.js
+++ trees/872_leaf_similar_trees.js
@@ -2,25 +2,23 @@
 
 const Node = require("./node_class").TreeNode;
 
-const leafSimilar = (root1, root2) => {};
+const leafSimilar = (root1, root2) => {
+  if (!root1 || !root2) return true;
 
-const root1 = new Node(3);
-root1.left = new Node(5);
-root1.left.left = new Node(6);
-root1.left.right = new Node(2);
-root1.left.right.left = new Node(7);
-root1.right = new Node(1);
-root1.right.right = new Node(8);
-root1.right.left = new Node(9);
+  console.log(root1.val);
+  console.log(root2.val);
+  console.log("------");
 
-const root2 = new Node(3);
-root2.left = new Node(5);
-root2.left.left = new Node(6);
-root2.left.right = new Node(7);
-root2.right = new Node(1);
-root2.right.left = new Node(4);
-root2.right.right = new Node(2);
-root2.right.right.left = new Node(9);
-root2.right.right.right = new Node(8);
+  leafSimilar(root1.left, root2.left);
+  leafSimilar(root1.right, root2.right);
+};
 
-console.log(root1);
+const root1 = new Node(1);
+root1.left = new Node(2);
+root1.right = new Node(3);
+
+const root2 = new Node(1);
+root2.left = new Node(3);
+root2.right = new Node(2);
+
+leafSimilar(root1, root2);
