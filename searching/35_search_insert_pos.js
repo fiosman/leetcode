2bb7ddd8b4abaa7d8889683e41545220b015b482
fiosman <fares.osman@outlook.com>
2021-07-12 23:58:37 -0400

    Upload 35 with time and space complexity

--- searching/35_search_insert_pos.js
+++ searching/35_search_insert_pos.js
@@ -1,10 +1,11 @@
 // https://leetcode.com/problems/search-insert-position/
 
-// Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+// Given a sorted array of distinct integers and a target value, return the index if the target is found.
 // If not, return the index where it would be if it were inserted in order.
 
 // You must write an algorithm with O(log n) runtime complexity.
 
-const searchInsert = (nums, traget) => { 
-    
-}
+const searchInsert = (nums, traget) => {
+  //binary search since array is sorted
+  // O(log n) time and O(log n) space
+};
