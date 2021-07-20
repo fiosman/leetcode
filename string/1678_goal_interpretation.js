e155f7dd0e6eed68707a433d89c85c214b79cd92
fiosman92 <fares.osman@outlook.com>
2021-07-19 23:47:29 -0400

    Upload 1678

---
+++ string/1678_goal_interpretation.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/goal-parser-interpretation/
+
+// Input: command = "G()(al)"
+// Output: "Goal"
+// Explanation: The Goal Parser interprets the command as follows:
+// G -> G
+// () -> o
+// (al) -> al
+// The final concatenated result is "Goal".
+// Example 2:
+
+// Input: command = "G()()()()(al)"
+// Output: "Gooooal"
+// Example 3:
+
+// Input: command = "(al)G(al)()()G"
+// Output: "alGalooG"
+
+const interpret = (command) => {};
