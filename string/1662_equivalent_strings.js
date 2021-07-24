e145302ee549636624215e2cfba793c457fba10f
fiosman92 <fares.osman@outlook.com>
2021-07-24 12:08:55 -0400

    Finish 1662

--- string/1662_equivalent_strings.js
+++ string/1662_equivalent_strings.js
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
 
 const arrayStringsAreEqual = (word1, word2) => {
+  //O(n) time and O(n) space
   return word1.join("") === word2.join("");
 };
