a058ab50cba9f174e76365bacf145ab4705b5eea
fiosman92 <fares.osman@outlook.com>
2021-08-02 00:47:19 -0400

    Finish 1374

--- string/1374_string_odd_counts.js
+++ string/1374_string_odd_counts.js
@@ -1,3 +1,19 @@
 // https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
 
-const generateTheString = (n) => {};
+const generateTheString = (n) => {
+  let str = "";
+  if (n % 2 != 0) {
+    for (let i = 0; i < n; i++) {
+      str = str.concat("a");
+    }
+  } else {
+    for (let i = 0; i < n - 1; i++) {
+      str = str.concat("a");
+    }
+    str = str.concat("b");
+  }
+
+  return str;
+};
+
+console.log(generateTheString(5));
