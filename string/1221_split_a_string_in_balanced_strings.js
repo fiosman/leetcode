eec7d1bc0c2a7e1a254fbf76e11dd3fdfff40fc5
fiosman92 <fares.osman@outlook.com>
2021-08-02 22:23:01 -0400

    Finish 1221

--- string/1221_split_a_string_in_balanced_strings.js
+++ string/1221_split_a_string_in_balanced_strings.js
@@ -1,3 +1,27 @@
 // https://leetcode.com/problems/split-a-string-in-balanced-strings/
 
-const balancedStringSplit = (s) => {};
+const balancedStringSplit = (s) => {
+  let numOfL = 0;
+  let numOfR = 0;
+  let total = 0;
+
+  for (let i = 0; i < s.length + 1; i++) {
+    if (numOfL > 0 && numOfR > 0 && numOfL === numOfR) {
+      total++;
+      numOfL = 0;
+      numOfR = 0;
+    }
+    if (s[i] === "R") {
+      numOfR++;
+    } else {
+      numOfL++;
+    }
+  }
+
+  return total;
+};
+
+console.log(balancedStringSplit("RLRRLLRLRL"));
+console.log(balancedStringSplit("RLLLLRRRLR"));
+console.log(balancedStringSplit("LLLLRRRR"));
+console.log(balancedStringSplit("RLRRRLLRLL"));
