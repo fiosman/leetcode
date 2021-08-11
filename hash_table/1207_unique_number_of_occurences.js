149db9eb8f89716516e46cfecafe40e971bcc2f3
fiosman92 <fares.osman@outlook.com>
2021-08-10 23:57:27 -0400

    Finish 1207

--- hash_table/1207_unique_number_of_occurences.js
+++ hash_table/1207_unique_number_of_occurences.js
@@ -8,4 +8,20 @@
 // Output: true
 // Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
 
-const uniqueOccurrences = (arr) => {};
+const uniqueOccurrences = (arr) => {
+  //O(n) time and O(n) space
+  const freq = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (freq[arr[i]]) {
+      freq[arr[i]]++;
+    } else {
+      freq[arr[i]] = 1;
+    }
+  }
+
+  const set = new Set(Object.values(freq));
+  return Object.values(freq).length == [...set].length ? true : false;
+};
+
+console.log(uniqueOccurrences([1, 2, 2, 1, 1, 3]));
