294dc6de65b1ab95050b6f27cceb0e95af3d9c4b
fiosman <fares.osman@outlook.com>
2023-04-29 23:37:47 -0400

    adds solution

--- string/58_length_of_last_word.js
+++ string/58_length_of_last_word.js
@@ -3,4 +3,7 @@
 // Given a string s consisting of words and spaces, return the length of the last word in the string.
 // A word is a maximal substring consisting of non-space characters only.
 
-const lengthOfLastWord = (string) => {};
+const lengthOfLastWord = (string) => {
+  const words = string.trim().split(" ");
+  return words[words.length - 1].length;
+};
