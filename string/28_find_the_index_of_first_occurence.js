2f005d58b9ba1dbe6055f5344b2622c4f330ddc6
fiosman <fares.osman@outlook.com>
2023-05-01 23:19:33 -0400

    starts on problem

---
+++ string/28_find_the_index_of_first_occurence.js
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack
+// or -1 if needle is not part of haystack.
+
+const strStr = (haystack, needle) => {};
