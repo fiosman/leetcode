13aaeaa45b14b4aaaf0d46738f72a4254919033d
fiosman <fares.osman@outlook.com>
2023-05-03 22:56:56 -0400

    partially working solution

--- string/28_find_the_index_of_first_occurence.js
+++ string/28_find_the_index_of_first_occurence.js
@@ -3,4 +3,15 @@
 // Given two strings needle and haystack, return the index of the first occurrence of needle in haystack
 // or -1 if needle is not part of haystack.
 
-const strStr = (haystack, needle) => {};
+const strStr = (haystack, needle) => {
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle[0]) {
+      const subString = haystack.substring(i, needle.length);
+      if (subString === needle) {
+        return i;
+      }
+    } else {
+      return -1;
+    }
+  }
+};
