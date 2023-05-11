30a70dd437972a32f2e0963638d9a0b755c06c47
fiosman <fares.osman@outlook.com>
2023-05-10 23:50:31 -0400

    start on problem

--- string/67_add_binary.js
+++ string/67_add_binary.js
@@ -1,3 +1,8 @@
 // https://leetcode.com/problems/add-binary/
 
 // Given two binary strings a and b, return their sum as a binary string.
+
+function addBinary(a, b) {}
+
+console.log(addBinary("11", "1"));
+console.log(addBinary("1010", "1011"));
