dfd8c23c8263606a2ea8be04e70d90a223468819
fiosman <fares.osman@outlook.com>
2023-05-12 22:28:33 -0400

    adds temp solution

--- string/67_add_binary.js
+++ string/67_add_binary.js
@@ -2,7 +2,10 @@
 
 // Given two binary strings a and b, return their sum as a binary string.
 
-function addBinary(a, b) {}
+function addBinary(a, b) {
+  const decSum = parseInt(a, 2) + parseInt(b, 2);
+  return decSum.toString(2);
+}
 
 console.log(addBinary("11", "1"));
 console.log(addBinary("1010", "1011"));
