5f83a3457ab4f00f219eba95fc3abdbc42f4ef6f
fiosman <fares.osman@outlook.com>
2023-05-24 23:55:27 -0400

    adds progress to problem

--- string/1768_merge_strings_alternate.js
+++ string/1768_merge_strings_alternate.js
@@ -24,3 +24,14 @@
 // word1:  a   b   c   d
 // word2:    p   q
 // merged: a p b q c   d
+
+const mergeAlternately = (word1, word2) => {
+  let finalWord = "";
+  for (let i = 0; i < word1.length; i++) {
+    finalWord += word1[i] + word2[i];
+  }
+
+  console.log(finalWord);
+};
+
+return mergeAlternately("ab", "pqrs");
