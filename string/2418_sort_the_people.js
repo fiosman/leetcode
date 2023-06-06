c5a72d3359538967d9ee3a6217dfbf2cf7d77c51
fiosman <fares.osman@outlook.com>
2023-06-05 23:46:26 -0400

    starto n problem

---
+++ string/2418_sort_the_people.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/sort-the-people/
+
+// Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+// Output: ["Mary","Emma","John"]
+// Explanation: Mary is the tallest, followed by Emma and John.
+
+// Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+// Output: ["Bob","Alice","Bob"]
+// Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+
+const sortPeople = (names, heights) => {};
