d93ab4228ebd3c1981b7be14d465eea0a6777bd6
fiosman <fares.osman@outlook.com>
2023-06-14 23:54:51 -0400

    attempts to solve probleM

--- string/2710_remove_trailing_zeros.js
+++ string/2710_remove_trailing_zeros.js
@@ -11,5 +11,13 @@
 // Explanation: Integer "123" has no trailing zeros, we return integer "123".
 
 const removeTrailingZeros = (num) => {
-  if (num[num.length - 1] !== 0) return num;
+  if (num[num.length - 1] !== "0") return num;
+
+  for (let i = num.length - 1; i > 0; i--) {
+    if (num[i] === "0") {
+      num = num.slice(0, num.length - 1);
+    }
+  }
+
+  return num;
 };
