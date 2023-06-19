f7a82155aa8c3054675c48192d5e938b3a74e90f
fiosman <fares.osman@outlook.com>
2023-06-19 00:03:31 -0400

    adds final solution

--- string/2710_remove_trailing_zeros.js
+++ string/2710_remove_trailing_zeros.js
@@ -13,15 +13,14 @@
 const removeTrailingZeros = (num) => {
   if (num[num.length - 1] !== "0") return num;
 
-  let noTrailingZeroes = "";
-
   for (let i = num.length - 1; i >= 0; i--) {
     if (num[i] === "0") {
       num = num.slice(0, num.length - 1);
+      continue;
+    } else {
+      break;
     }
   }
 
-  return noTrailingZeroes;
+  return num;
 };
-
-console.log(removeTrailingZeros("51230100"));
