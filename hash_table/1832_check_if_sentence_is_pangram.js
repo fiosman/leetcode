7af634e28e2718834bf623806e1579cd9053e266
fiosman <fares.osman@outlook.com>
2023-06-19 22:50:34 -0400

    finish problem

--- hash_table/1832_check_if_sentence_is_pangram.js
+++ hash_table/1832_check_if_sentence_is_pangram.js
@@ -1,3 +1,11 @@
 // https://leetcode.com/problems/check-if-the-sentence-is-pangram/
 
-var checkIfPangram = function (sentence) {};
+var checkIfPangram = function (sentence) {
+  const uniqueSentence = new Set(sentence);
+
+  if (uniqueSentence.size < 26) {
+    return false;
+  } else {
+    return true;
+  }
+};
