27ac4dc6845bc4353173d9c1344e8bd72e915e09
fiosman <fares.osman@outlook.com>
2023-06-22 22:50:51 -0400

    set solution

--- math/414_third_maximum_number.js
+++ math/414_third_maximum_number.js
@@ -1,3 +1,11 @@
 // https://leetcode.com/problems/third-maximum-number/
 
-var thirdMax = function (nums) {};
+var thirdMax = function (nums) {
+  const sortedUniqueNumsArray = Array.from(new Set(nums)).sort((a, b) =>
+    nums.length >= 3 ? b - a : a - b
+  );
+
+  return sortedUniqueNumsArray.length >= 3
+    ? sortedUniqueNumsArray[2]
+    : sortedUniqueNumsArray[sortedUniqueNumsArray.length - 1];
+};
