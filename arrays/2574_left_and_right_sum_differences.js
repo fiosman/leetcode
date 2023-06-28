a96f958e40bac1870f67ba7bc4a5dd4922e31268
fiosman <fares.osman@outlook.com>
2023-06-27 23:44:34 -0400

    Begin problem

--- arrays/2574_left_and_right_sum_differences.js
+++ arrays/2574_left_and_right_sum_differences.js
@@ -1,3 +1,8 @@
 // https://leetcode.com/problems/left-and-right-sum-differences/
 
-var leftRightDifference = function (nums) {};
+var leftRightDifference = function (nums) {
+  const leftSum = [];
+  const rightSum = [];
+
+  for (let i = 0; i < nums.length; i++) {}
+};
