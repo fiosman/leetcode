6038664ab08a0a3448abe64fafa6997061dd74de
fiosman <fares.osman@outlook.com>
2023-06-28 23:36:10 -0400

    adds logic for left sum

--- arrays/2574_left_and_right_sum_differences.js
+++ arrays/2574_left_and_right_sum_differences.js
@@ -1,8 +1,15 @@
 // https://leetcode.com/problems/left-and-right-sum-differences/
 
 var leftRightDifference = function (nums) {
-  const leftSum = [];
+  let currentLeftSum = 0;
+  let currentRightSum = 0;
+  const leftSum = [0];
   const rightSum = [];
 
-  for (let i = 0; i < nums.length; i++) {}
+  for (let i = 1; i < nums.length; i++) {
+    currentLeftSum += nums[i - 1];
+    leftSum.push(currentLeftSum);
+  }
 };
+
+leftRightDifference([10, 4, 8, 3]);
