07eb0ef32cc3c5c1311328020d09a67e409839b1
fiosman <fares.osman@outlook.com>
2023-07-05 22:25:35 -0400

    Bro what

--- arrays/2643_row_with_maximum_ones.js
+++ arrays/2643_row_with_maximum_ones.js
@@ -1,14 +1,27 @@
 // https://leetcode.com/problems/row-with-maximum-ones/
 
 const rowAndMaximumOnes = (mat) => {
-  const maximumOnes = [];
   let currentRowOneCount = 0;
+  const finalRowCounts = [];
 
   for (let i = 0; i < mat.length; i++) {
     for (let j = 0; j < i.length; j++) {
+      console.log(mat[i][j]);
       if (mat[i][j] === 1) {
         currentRowOneCount++;
       }
     }
+
+    finalRowCounts.push(currentRowOneCount);
+    currentRowOneCount = 0;
   }
+
+  return finalRowCounts;
 };
+
+console.log(
+  rowAndMaximumOnes([
+    [0, 1],
+    [1, 0],
+  ])
+);
