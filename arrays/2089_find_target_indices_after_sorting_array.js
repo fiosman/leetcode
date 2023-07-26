15f6d134893f4da1959cc8bb726a7932b3fcd746
fiosman <fares.osman@outlook.com>
2023-07-25 23:50:56 -0400

    set up problem statement

---
+++ arrays/2089_find_target_indices_after_sorting_array.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/find-target-indices-after-sorting-array/
+
+// Example 1:
+
+// Input: nums = [1,2,5,2,3], target = 2
+// Output: [1,2]
+// Explanation: After sorting, nums is [1,2,2,3,5].
+// The indices where nums[i] == 2 are 1 and 2.
+
+// Example 2:
+
+// Input: nums = [1,2,5,2,3], target = 3
+// Output: [3]
+// Explanation: After sorting, nums is [1,2,2,3,5].
+// The index where nums[i] == 3 is 3.
+
+// Example 3:
+
+// Input: nums = [1,2,5,2,3], target = 5
+// Output: [4]
+// Explanation: After sorting, nums is [1,2,2,3,5].
+// The index where nums[i] == 5 is 4.
+
+const targetIndices = (nums, target) => {};
