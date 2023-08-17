c43a49c97f368170bfd7e48b42309f1602f038c6
fiosman <fares.osman@outlook.com>
2023-08-16 22:21:55 -0400

    template

---
+++ arrays/283_move_zeroes.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/move-zeroes/
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+const moveZeroes = (nums) => {};
