86aebb9b39c0d8451cf75333f35c586e3a4f5033
fiosman <fares.osman@outlook.com>
2023-08-29 22:58:08 -0400

    got a working solution

--- arrays/283_move_zeroes.js
+++ arrays/283_move_zeroes.js
@@ -10,15 +10,14 @@
 // Output: [0]
 
 const moveZeroes = (nums) => {
-  for (let i = 0; i < nums.length; i++) {}
+  let nonZeroIndex = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      [nums[nonZeroIndex], nums[i]] = [nums[i], nums[nonZeroIndex]];
+      nonZeroIndex++;
+    }
+  }
 
   return nums;
-};
-
-const swapEles = (eles) => {
-  const temp = eles[0];
-  eles[0] = eles[1];
-  eles[1] = temp;
-
-  return eles;
 };
