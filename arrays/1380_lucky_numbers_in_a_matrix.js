a71978c12827e53de02be10e6fc4cf4cb65e8957
fiosman <fares.osman@outlook.com>
2023-09-01 23:57:14 -0400

    uhm we doing it

--- arrays/1380_lucky_numbers_in_a_matrix.js
+++ arrays/1380_lucky_numbers_in_a_matrix.js
@@ -18,4 +18,28 @@
 // Output: [7]
 // Explanation: 7 is the only lucky number since it is the minimum in its row and the maximum in its column.
 
-const luckyNumbers = (matrix) => {};
+const luckyNumbers = (matrix) => {
+  let maxValPosCol = {};
+  let minValPosRow = {};
+
+  for (let i = 0; i < matrix.length; i++) {
+    let currentMinRow = matrix[i][0];
+    minValPosRow[currentMinRow] = i;
+    for (let j = 1; j < matrix[i].length; j++) {
+      console.log(`oh yes`, matrix[i][j]);
+      if (matrix[i][j] < currentMinRow) {
+        currentMinRow = matrix[i][j];
+        minValPosRow[currentMinRow] = i;
+      }
+    }
+  }
+  return minValPosRow;
+};
+
+console.log(
+  luckyNumbers([
+    [3, 7, 8],
+    [9, 11, 13],
+    [15, 16, 17],
+  ])
+);
