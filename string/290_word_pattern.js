f248fc655596aed17e0b047d466cf9fadb5dffd6
fiosman <fares.osman@outlook.com>
2023-09-14 23:13:31 -0400

    adds template

---
+++ string/290_word_pattern.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/word-pattern/
+
+// Example 1:
+
+// Input: pattern = "abba", s = "dog cat cat dog"
+// Output: true
+
+// Example 2:
+
+// Input: pattern = "abba", s = "dog cat cat fish"
+// Output: false
+
+// Example 3:
+
+// Input: pattern = "aaaa", s = "dog cat cat dog"
+// Output: false
+
+const wordPattern = (pattern, s) => {};
