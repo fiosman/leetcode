099fbccadd6f5d5c9bb6d8639f444009d53983fe
fiosman <fares.osman@outlook.com>
2023-09-15 23:59:43 -0400

    breh

--- string/290_word_pattern.js
+++ string/290_word_pattern.js
@@ -15,4 +15,14 @@
 // Input: pattern = "aaaa", s = "dog cat cat dog"
 // Output: false
 
-const wordPattern = (pattern, s) => {};
+const wordPattern = (pattern, s) => {
+  let stringPattern = "";
+  const modifiedString = s.split(" ");
+  for (let i = 0; i < s.length; i++) {
+    stringPattern += s[i][0];
+  }
+
+  return stringPattern;
+};
+
+console.log(wordPattern("abba", "dog cat cat dog"));
