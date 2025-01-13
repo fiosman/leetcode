f356bb2e8ad471bda7f6f5e126034b440aba02d8
fiosman <fares.osman@outlook.com>
2025-01-13 13:11:25 -0500

    feat: finish problem

--- string/2942_find_words_containing_character.py
+++ string/2942_find_words_containing_character.py
@@ -5,7 +5,6 @@ class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         array = []
         for i in range(len(words)):
-            print(i)
             if x in words[i]:
                 array.append(i)
         return array
