8bb8b574cff921dfba88b3ef8b921bc3b3e8463d
fiosman <fares.osman@outlook.com>
2025-01-22 22:17:25 -0500

    feat: finish exercise

---
+++ arrays/451_sort_characters_by_frequency.py
@@ -0,0 +1,16 @@
+from collections import Counter
+import heapq
+
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        map = Counter(s)
+        negated_frequencies = [(-freq, char) for char, freq in map.items()]
+        heapq.heapify(negated_frequencies)
+
+        sorted_str = ""
+        while negated_frequencies:
+            popped = heapq.heappop(negated_frequencies)
+            print(popped[1])
+            sorted_str += popped[1] * -popped[0]
+        return sorted_str
