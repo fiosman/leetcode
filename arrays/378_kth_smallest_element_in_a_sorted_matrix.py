7912575fbb70c3550e240dc710a40eccf9d50c0c
fiosman <fares.osman@outlook.com>
2025-01-22 21:10:13 -0500

    feat: finish exercise

---
+++ arrays/378_kth_smallest_element_in_a_sorted_matrix.py
@@ -0,0 +1,16 @@
+from typing import List
+import heapq
+
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        flattened = [item for sublist in matrix for item in sublist]
+        heapq.heapify(flattened)
+
+        for _ in range(k):
+            min = heapq.heappop(flattened)
+        return min
+
+
+soln = Solution()
+print(soln.kthSmallest([[-5]], 1))
