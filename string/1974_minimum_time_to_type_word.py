5a03f76f0d97ffde0598bbb1c0995b18abda37a0
fiosman <fares.osman@outlook.com>
2025-03-18 21:36:40 -0400

    feat: finish exercise

---
+++ string/1974_minimum_time_to_type_word.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minTimeToType(self, word: str) -> int:
+      total_seconds = min(abs(ord(word[0]) - ord('a')), 26 - abs(ord(word[0]) - ord('a'))) + 1
+      for i in range(1, len(word)):
+        moves = min(abs(ord(word[i]) - ord(word[i - 1])), 26 - abs(ord(word[i]) - ord(word[i - 1])))
+        total_seconds += moves + 1
+      
+      return total_seconds
+      
+    
+    
+    
+    
+    
